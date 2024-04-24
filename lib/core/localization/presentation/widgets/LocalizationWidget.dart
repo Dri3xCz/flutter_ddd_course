790d93af50976c3788c41d5e96978ed40aff598b
Dri3xCz <Dri3xCz@gmail.com>
2024-04-24 15:32:57 +0200

    refactor: changed LocalizationWidget to use HookWidget instead of StatefullWidget

--- lib/core/localization/presentation/widgets/LocalizationWidget.dart
+++ lib/core/localization/presentation/widgets/LocalizationWidget.dart
@@ -1,35 +1,27 @@
 
 import 'package:clean_flutter_tdd_ddd/core/localization/domain/entities/localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class LocalizationWidget extends StatefulWidget {
+class LocalizationWidget extends HookWidget {
 
-  @override
-  State<LocalizationWidget> createState() => _LocalizationWidgetState();
-}
-
-class _LocalizationWidgetState extends State<LocalizationWidget> {
-  List<LocalizationLanguage> languagesArray = LocalizationLanguage.values;
+  final List<LocalizationLanguage> languagesArray = LocalizationLanguage.values;
   
-  // State that will be handled by redux in future
-  LocalizationLanguage currentLanguage = LocalizationLanguage.English;
-  int currentIndex = 0;
-
-  LocalizationLanguage nextLanguage() {
-    currentIndex = currentIndex >= languagesArray.length - 1 
-      ? 0 
-      : currentIndex + 1;
-    return languagesArray[currentIndex];
-  } 
-
-  void _onPressed() {
-    setState(() {
-      currentLanguage = nextLanguage();
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    final currentLanguageIndex = useState(0);
+    final currentLanguage = languagesArray[currentLanguageIndex.value];
+
+    int nextLanguageIndex() {
+      return currentLanguageIndex.value = currentLanguageIndex.value >= languagesArray.length - 1 
+        ? 0 
+        : currentLanguageIndex.value + 1;
+    } 
+
+    void _onPressed() {
+      currentLanguageIndex.value = nextLanguageIndex();
+    }
+
     return Container(
       width: double.infinity,
       child: Padding(
