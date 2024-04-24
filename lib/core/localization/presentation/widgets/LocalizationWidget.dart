71a1319ca0b4ba08422ff751f6e9b9660c3a9223
Dri3xCz <Dri3xCz@gmail.com>
2024-04-24 16:20:26 +0200

    refactor: factored out logic out of LocalizationWidget into ListQueue Rotate extension

--- lib/core/localization/presentation/widgets/LocalizationWidget.dart
+++ lib/core/localization/presentation/widgets/LocalizationWidget.dart
@@ -1,46 +1,53 @@
 
+import 'dart:collection';
+
 import 'package:clean_flutter_tdd_ddd/core/localization/domain/entities/localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
+extension Rotate on ListQueue {
+  dynamic rotateLeft() {
+    final value = this.removeFirst();
+ 
+    this.addLast(value);
+ 
+    return value;
+  }
+}
+
 class LocalizationWidget extends HookWidget {
 
-  final List<LocalizationLanguage> languagesArray = LocalizationLanguage.values;
-  
+  final ListQueue<LocalizationLanguage> languageQueue = ListQueue.from(LocalizationLanguage.values);
+  late LocalizationLanguage initialState;
+
+  LocalizationWidget() {
+    initialState = languageQueue.rotateLeft();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentLanguageIndex = useState(0);
-    final currentLanguage = languagesArray[currentLanguageIndex.value];
-
-    int nextLanguageIndex() {
-      return currentLanguageIndex.value = currentLanguageIndex.value >= languagesArray.length - 1 
-        ? 0 
-        : currentLanguageIndex.value + 1;
-    } 
+    final currentLanguage = useState(initialState);
 
     void _onPressed() {
-      currentLanguageIndex.value = nextLanguageIndex();
+      currentLanguage.value = languageQueue.rotateLeft();
     }
 
     return Container(
       width: double.infinity,
       child: Padding(
         padding: EdgeInsets.all(10),
-        child: ElevatedButton.icon(
+        child: OutlinedButton.icon(
           onPressed: _onPressed,
           icon: Icon(Icons.language),
           label: Container(
             width: double.infinity,
             child: Text(
-              currentLanguage.languageCode.toUpperCase(),
+              currentLanguage.value.languageCode.toUpperCase(),
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 16,
               ),
             ),
-          ),
-          style: ButtonStyle(
-            side: MaterialStateProperty.all(BorderSide(width: 1))
           ),
         ),
       ),
